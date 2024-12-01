036ee1f6ae9f2e5eca9c33e1df5178f0ab12a9ec
Md. Adib Khan <142529792+WebAdib@users.noreply.github.com>
2024-12-01 20:53:54 +0600

    final work

--- lib/home.dart
+++ lib/home.dart
@@ -10,9 +10,28 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   Color _selectedColor = Colors.blue;
+
+  double _radius = 25.0;
+
   Future<void> chooseColor() async {
-    Color? color = await showColorPickerDialog(context, _selectedColor,
-        title: Text('Choose Color'));
+    Color? color = await showColorPickerDialog(
+      context,
+      _selectedColor,
+      title: Text('Choose Color'),
+      showColorCode: true,
+      showColorName: true,
+      showRecentColors: true,
+      borderRadius: _radius,
+      pickersEnabled: <ColorPickerType, bool>{
+        ColorPickerType.wheel: true,
+      },
+      subheading: Text("Shades"),
+    );
+    if (color != null) {
+      setState(() {
+        _selectedColor = color;
+      });
+    }
   }
 
   @override
@@ -23,7 +42,50 @@ class _HomeState extends State<Home> {
         backgroundColor: _selectedColor,
         title: Text('Home Page'),
       ),
-      body: Column(),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 15.0, left: 15),
+            child: Text(
+              'Set Radious',
+              style: TextStyle(fontSize: 18, color: Colors.black),
+            ),
+          ),
+          Slider(
+            value: _radius,
+            max: 25,
+            activeColor: _selectedColor,
+            thumbColor: _selectedColor,
+            onChanged: (value) {
+              setState(() {
+                _radius = value;
+              });
+            },
+          ),
+          ColorPicker(
+            showColorCode: true,
+            showColorName: true,
+            showMaterialName: true,
+            colorNameTextStyle: TextStyle(fontWeight: FontWeight.bold),
+            showRecentColors: true,
+            enableOpacity: true,
+            color: _selectedColor,
+            borderRadius: _radius,
+            pickersEnabled: <ColorPickerType, bool>{
+              ColorPickerType.wheel: true,
+            },
+            subheading: Text("Shades"),
+            onColorChanged: (value) {
+              setState(
+                () {
+                  _selectedColor = value;
+                },
+              );
+            },
+          ),
+        ],
+      ),
       floatingActionButton: FloatingActionButton(
           backgroundColor: _selectedColor,
           child: Icon(
